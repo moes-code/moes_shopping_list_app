0656d9582092baaaa8f2c483fa4094c60553f14e
Moritz <git.stroller152@slmails.com>
2025-06-08 20:40:21 +0200

    Add project files.

---
+++ moes_shopping_list_app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace moes_shopping_list_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ moes_shopping_list_app/Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace moes_shopping_list_app
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
