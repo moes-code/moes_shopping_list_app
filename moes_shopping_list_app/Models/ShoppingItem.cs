8ea63d892935dc6816fa3a1f3a542ba03d90994f
Moritz <git.stroller152@slmails.com>
2025-06-10 22:27:42 +0200

    Added new model shoppingitem

---
+++ moes_shopping_list_app/Models/ShoppingItem.cs
@@ -0,0 +1,15 @@
+﻿// Define a namespace for the shopping list application models
+namespace moes_shopping_list_app.Models
+{
+    // Define a class to represent an item in the shopping list
+    public class ShoppingItem
+    {
+        // Property to store the unique identifier for the shopping item
+        public int Id { get; set; }
+        // Property to store the name of the shopping item
+        // The 'required' keyword indicates that this property must be provided
+        public required string Name { get; set; }
+        // Property to store the quantity of the shopping item
+        public int Quantity { get; set; }
+    }
+}
