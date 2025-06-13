9a63b12656cfc03eb3b3fc077db09ef4bdc3e5c7
Moritz <git.stroller152@slmails.com>
2025-06-13 12:43:36 +0200

    Set the BindingContext of the main page to the view model

--- moes_shopping_list_app/MauiProgram.cs
+++ moes_shopping_list_app/MauiProgram.cs
@@ -1,24 +1,44 @@
-﻿using Microsoft.Extensions.Logging;
+﻿// Importing the ShoppingItemRepository class from the Repositories namespace
+using moes_shopping_list_app.Repositories;
+// Importing the ShoppingListViewModel class from the ViewModels namespace
+using moes_shopping_list_app.ViewModels;
+// Importing the logging extensions from Microsoft.Extensions.Logging
+using Microsoft.Extensions.Logging;
 
+// Declaring the namespace for the application
 namespace moes_shopping_list_app
 {
+    // Defining a static class named MauiProgram
     public static class MauiProgram
     {
+        // Method to create and configure a MauiApp instance
         public static MauiApp CreateMauiApp()
         {
+            // Creating a new MauiAppBuilder instance
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
 
+            // Configuring the Maui application with the main App class and registering services
+            builder
+                .UseMauiApp<App>() // Specifies the main application class
+                .Services.AddSingleton<ShoppingItemRepository>() // Registers ShoppingItemRepository as a singleton service
+                .AddTransient<ShoppingListViewModel>(); // Registers ShoppingListViewModel as a transient service
+
+            // Configuring fonts for the application
+            builder.ConfigureFonts(fonts =>
+            {
+                // Adding the OpenSans-Regular font with the alias "OpenSansRegular"
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                // Adding the OpenSans-Semibold font with the alias "OpenSansSemibold"
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+            // Conditional compilation directive for debug mode
 #if DEBUG
-    		builder.Logging.AddDebug();
+            // Adding debug logging if the application is in debug mode
+            builder.Logging.AddDebug();
 #endif
 
+            // Building and returning the configured MauiApp instance
             return builder.Build();
         }
     }
--- moes_shopping_list_app/Views/MainPage.xaml.cs
+++ moes_shopping_list_app/Views/MainPage.xaml.cs
@@ -1,9 +1,20 @@
-namespace moes_shopping_list_app.Views;
+// Importing the ShoppingListViewModel class from the ViewModels namespace
+using moes_shopping_list_app.ViewModels;
 
-public partial class MainPage : ContentPage
+// Declaring the namespace for the Views in the application
+namespace moes_shopping_list_app.Views
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    // Defining a partial class named MainPage that inherits from ContentPage
+    public partial class MainPage : ContentPage
+    {
+        // Constructor for the MainPage class that takes a ShoppingListViewModel as a parameter
+        public MainPage(ShoppingListViewModel viewModel)
+        {
+            // Initializes the components of the page (UI elements defined in XAML)
+            InitializeComponent();
+
+            // Sets the BindingContext of the page to the provided view model
+            BindingContext = viewModel;
+        }
+    }
 }
